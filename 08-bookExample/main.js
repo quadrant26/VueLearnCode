80d37b260625a08b957dc8b35af8d1da74354cda
quadrant26 <quadrant26@163.com>
2020-01-07 11:37:57 +0800

    ES6 高阶函数 filter map reduce

--- 08-bookExample/main.js
+++ 08-bookExample/main.js
@@ -35,13 +35,29 @@ const app = new Vue({
     computed:{
         totalAllPrice (){
             let totalPrice = 0;
-            for(let i = 0; i < this.list.length; i++){
-                totalPrice += this.list[i].count * this.list[i].price;
-            }
-            return totalPrice;
+
+            // 1. 普通的 for 循环
+            // for(let i = 0; i < this.list.length; i++){
+            //     totalPrice += this.list[i].count * this.list[i].price;
+            // }
 
             // for (let i in/of this.list)
-            // reduce
+            // 2. for in
+            // for (let i in this.list ){
+            //     totalPrice += this.list[i].count * this.list[i].price;
+            // }
+
+            // 3. for item of
+            // for( let item of this.list){
+            //     totalPrice += item.count * item.price;
+            // }
+
+            // 4. reduce
+            totalPrice = this.list.reduce(function (preValue, book){
+                return preValue + book.count * book.price;
+            }, 0)
+
+            return totalPrice;
         }
     },
     methods: {
@@ -68,3 +84,69 @@ const app = new Vue({
         }
     }
 })
+
+const nums = [10, 20, 111, 222, 444, 40, 50]
+// 函数式编程
+let totals = nums.filter(function(n){
+    return n < 100;
+}).map(function (n){
+    return n * 2;
+}).reduce(function (preValue, n){
+    return preValue+n;
+});
+console.log(totals);
+
+let totals2 = nums.filter(n => n < 100).map( n => n*2).reduce((pre, n) => pre+n, 0);
+console.log(totals2)
+
+
+// filter/map/reduce
+
+// 1. filter 回调函数必须有一个要求： 必须返回一个布尔值
+// true: 当返回true，函数内部自动将这次回调的n加入到新的数组中
+// false: 返回false， 将会过滤这次的 n
+let new4Nums = nums.filter(function (n){
+    return n < 100;
+})
+console.log(new4Nums)
+
+// 2. map
+let new3Nums = nums.map(function (n){
+    return n * 2;
+});
+console.log(new3Nums);
+
+// 3. reduce
+// 对数组中的内容进行汇总
+let new5Nums = nums.reduce(function(preValue, n){
+    return preValue + n;
+}, 0);
+console.log(new5Nums);
+
+// 第一次： preValue -> 0, n -> 10
+// 第二次： preValue -> 第一次的返回值, n -> 20
+// 第三次： preValue -> 第二次的返回值, n -> 111
+// 第四次： preValue -> 第三次的返回值, n -> 222
+
+// 1. 取出所有的小于100的数字
+let newNums = [];
+for ( let n of nums ){
+    if( n < 100){
+        newNums.push(n)
+    }
+}
+console.log(newNums);
+
+// 2. 所有的数字 * 2
+let new2Nums = [];
+for ( let n of nums ){
+    new2Nums.push(n*2)
+}
+console.log(new2Nums)
+
+// 2. 所有的数字 * 2
+let total = 0;
+for ( let n of nums ){
+    total += n;
+}
+console.log(total)
