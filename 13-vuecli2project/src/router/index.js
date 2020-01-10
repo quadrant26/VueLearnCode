083b872cc0a7564495d4f919a96864ed2a493b81
quadrant26 <quadrant26@163.com>
2020-01-10 17:10:55 +0800

    设置路由的懒加载

--- 13-vuecli2project/src/router/index.js
+++ 13-vuecli2project/src/router/index.js
@@ -2,11 +2,35 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
-import Home from '@/components/Home'
-import About from '@/components/About'
-import User from '@/components/User'
+// import Home from '@/components/Home'
+// import About from '@/components/About'
+// import User from '@/components/User'
 
 Vue.use(Router)
+
+/*
+const routes = [
+  {
+    path: "/",
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/about',
+    component: About
+  },
+  {
+    path: '/user/:userId',
+    component: User
+  }
+]*/
+
+const Home = () => import("@/components/Home");
+const About = () => import("@/components/About");
+const User = () => import("@/components/User");
 
 const routes = [
   {
