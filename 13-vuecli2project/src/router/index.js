4d262e2edd227334df77e2bfb7736c6e8081c324
quadrant26 <quadrant26@163.com>
2020-01-10 15:42:10 +0800

    Home About 基础路由显示

--- 13-vuecli2project/src/router/index.js
+++ 13-vuecli2project/src/router/index.js
@@ -2,14 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import Home from '@/components/Home'
+import About from '@/components/About'
+
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+const routes = [
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/about',
+    component: About
+  },
+]
+
+const router = new Router({
+  routes
 })
+
+export default router
