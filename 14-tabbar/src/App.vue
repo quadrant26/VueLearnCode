3869ce2364dd04f21545135c997750a802b86826
quadrant26 <quadrant26@163.com>
2020-01-13 11:59:06 +0800

    tabbarItem active 设置

--- 14-tabbar/src/App.vue
+++ 14-tabbar/src/App.vue
@@ -4,18 +4,22 @@
     <TabBar>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/home_active.svg" alt="">
         <div slot="item-text">首页</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/category_active.svg" alt="">
         <div slot="item-text">分类</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/cart.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/cart_active.svg" alt="">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/profile_active.svg" alt="">
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </TabBar>
