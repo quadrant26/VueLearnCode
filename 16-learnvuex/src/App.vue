f530838e843767c2d8ba2a399186b37d30ae15f5
quadrant26 <quadrant26@163.com>
2020-01-13 17:12:05 +0800

    learnvuex init

---
+++ 16-learnvuex/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <h2>{{message}}</h2>
+    <p>{{counter}}</p>
+
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data (){
+    return {
+      message: "我是App组件",
+      counter: 0,
+    }
+  },
+  methods: {
+    decrement (){
+      this.counter --;
+    },
+    increment (){
+      this.counter ++;
+    }
+  }
+}
+</script>
+
+<style>
+</style>
