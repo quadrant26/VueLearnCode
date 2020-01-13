5939e63d95c1bb4efbb5686b39684a0363c9ae8b
quadrant26 <quadrant26@163.com>
2020-01-13 14:40:36 +0800

    tabbar 对应的组件初始化

--- 14-tabbar/src/main.js
+++ 14-tabbar/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App'
 
+import router from 'vue-router'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
 
---
+++ 14-tabbar/src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+// 1. 安装插件
+Vue.use(VueRouter);
+
+// 创建懒加载
+const Home = () => import('../views/home/Home');
+const Category = () => import('../views/category/Cagegory');
+const Cart = () => import('../views/cart/Cart');
+const Profile = () => import('../views/profile/Profile');
+
+// 2. 创建路由对象
+const routes = [
+  {
+    path: '',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/category',
+    component: Category
+  },
+  {
+    path: '/cart',
+    component: Cart
+  },
+  {
+    path: '/profile',
+    component: Profile
+  }
+]
+
+
+const router = new VueRouter({
+  routes,
+  mode: "history"
+})
+
+// 3. 导出
+export default router;
